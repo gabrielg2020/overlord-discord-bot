a7957db52a98fbe50e6a24a620848beeb74c99ca
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2024-05-07 14:57:21 +0100

    release: v-1.3

--- app/src/main.py
+++ app/src/main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from modules.timeout_handler import handle_timeout, handle_clear_timeouts
+from modules.text_processer_handler import preprocess_text
 from fuzzywuzzy import fuzz,process
 
 load_dotenv()
@@ -21,6 +22,7 @@ async def on_ready():
         print(e)
 
 @bot.tree.command(name='clearto')
+@commands.has_role('Owner')
 async def clear(interaction: discord.Interaction, member: discord.Member):
     if member is None:
         await interaction.response.send_message('Please mention a valid member.', ephemeral=True)
@@ -30,15 +32,31 @@ async def clear(interaction: discord.Interaction, member: discord.Member):
 
 @bot.event
 async def on_message(message):
+    # Don't listen to messages from bot or blu#8464
     if message.author == bot.user or message.author.id == 598261411709321216:
-      return
+        return
 
-    phrase_variations = ['heroes of the strom', 'hots']
+    # For each variation if likely hood >= 85 timeout
+    phrase_variations = ['heroes of the storm', 'hots']
 
+    content = await preprocess_text(message.content)
+    print(content)
+
+    total_weight = 0
     for variation in phrase_variations:
-        if fuzz.partial_ratio(variation, message.content.lower()) >= 85:
+        current_weight = fuzz.partial_ratio(variation, content)
+        print(current_weight)
+        total_weight += current_weight
+
+        if current_weight >= 95:
             await handle_timeout(message)
-            await bot.process_commands(message)
+            break
+
+        if total_weight >= 133:
+            await handle_timeout(message)
+
+    
+    await bot.process_commands(message)
 
 if __name__ == '__main__':
     bot.run(TOKEN)
--- app/src/modules/timeout_handler.py
+++ app/src/modules/timeout_handler.py
@@ -4,7 +4,7 @@ import json
 
 def get_user_timeouts():
   try:
-    with open('app/src/modules/user_timeouts.json', 'r') as file:
+    with open('/user_timeouts.json', 'r') as file:
       return json.load(file)
   except FileNotFoundError:
     return {}
@@ -12,7 +12,7 @@ def get_user_timeouts():
 def update_user_timeouts(dic):
   try:
     json_data = json.dumps(dic, indent=4)
-    with open('app/src/modules/user_timeouts.json', 'w') as file:
+    with open('/user_timeouts.json', 'w') as file:
       file.write(json_data)
   except Exception as e:
     print(f'Error whilst writing to user_timeouts.json: {e}')
@@ -33,8 +33,9 @@ async def handle_timeout(message):
 
   timeout_duration = 5 ** (user_timeouts[user_id])
 
+  print(user_timeouts)
   await message.channel.send(f"{message.author.mention} mentioned 'HOTS'! {message.author.mention} has mentioned 'HOTS' {user_timeouts[user_id]} times. As a punishment for they are timedout for {timeout_duration} seconds. Feel free to laugh at {message.author.mention}.")
-  await message.author.edit(timed_out_until=discord.utils.utcnow() + datetime.timedelta(seconds=timeout_duration))
+  #await message.author.edit(timed_out_until=discord.utils.utcnow() + datetime.timedelta(seconds=timeout_duration))
 
 async def handle_clear_timeouts(user_id):
   user_timeouts = get_user_timeouts()
